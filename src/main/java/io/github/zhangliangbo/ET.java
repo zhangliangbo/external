7f9830bfd3c726a6812e4330e8a556a9f7bf5f3c
zhangliangbo <2472110501@qq.com>
2023-01-01 18:09:10 +0800

    auto push 20230101

---
+++ src/main/java/io/github/zhangliangbo/ET.java
@@ -0,0 +1,9 @@
+package io.github.zhangliangbo;
+
+/**
+ * @author zhangliangbo
+ * @since 2022/12/31
+ */
+public class ET {
+
+}
---
+++ src/main/java/io/github/zhangliangbo/inner/Exec.java
@@ -0,0 +1,8 @@
+package io.github.zhangliangbo.inner;
+
+/**
+ * @author zhangliangbo
+ * @since 2023/1/1
+ */
+public class Exec {
+}
