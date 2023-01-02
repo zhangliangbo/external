3e3c6295decacfc51e5f6275a8d900d4846a9bec
zhangliangbo <2472110501@qq.com>
2023-01-02 12:06:12 +0800

    auto push 20230102

--- src/main/java/io/github/zhangliangbo/external/inner/Git.java
+++ src/main/java/io/github/zhangliangbo/external/inner/Git.java
@@ -5,8 +5,5 @@ package io.github.zhangliangbo.external.inner;
  * @since 2023/1/1
  */
 public class Git extends AbstractExternalExecutable {
-    @Override
-    public String getExecutable() {
-        return "D:\\scoop\\shims\\git.exe";
-    }
+
 }
--- src/main/java/io/github/zhangliangbo/external/inner/Scoop.java
+++ src/main/java/io/github/zhangliangbo/external/inner/Scoop.java
@@ -1,12 +1,17 @@
 package io.github.zhangliangbo.external.inner;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang3.tuple.Pair;
+
 /**
  * @author zhangliangbo
  * @since 2023/1/1
  */
 public class Scoop extends AbstractExternalExecutable {
-    @Override
-    public String getExecutable() {
-        return "D:\\scoop\\shims\\scoop.cmd";
+    public JsonNode apps() throws Exception {
+        Pair<Integer, String> list = execute("list");
+        return new ObjectNode(JsonNodeFactory.instance);
     }
 }
