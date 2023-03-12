fa65f64784c7d7a6ae75be72c1b0f603bf1765eb
zhangliangbo <2472110501@qq.com>
2023-03-12 11:45:59 +0800

    auto push 20230312

--- src/main/java/io/github/zhangliangbo/external/inner/Git.java
+++ src/main/java/io/github/zhangliangbo/external/inner/Git.java
@@ -1,5 +1,10 @@
 package io.github.zhangliangbo.external.inner;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Date;
+
 /**
  * @author zhangliangbo
  * @since 2023/1/1
@@ -9,4 +14,21 @@ public class Git extends AbstractExternalExecutable {
     public String getName() {
         return "git";
     }
+
+    public void newMergeBranch(String base, String merge) throws Exception {
+        Pair<Integer, String> execute = execute("checkout", merge);
+        System.out.println(execute.getRight());
+        execute = execute("pull", "--ff-only");
+        System.out.println(execute.getRight());
+        execute = execute("checkout", base);
+        System.out.println(execute.getRight());
+        execute = execute("pull", "--ff-only");
+        System.out.println(execute.getRight());
+        String newBranchName = base + "-merge-" + merge + "-" + DateFormatUtils.format(new Date(), "yyyyMMddHHmmss");
+        execute = execute("checkout", "-b", newBranchName);
+        System.out.println(execute.getRight());
+        execute = execute("push", "--set-upstream", "origin", newBranchName);
+        System.out.println(execute.getRight());
+    }
+
 }
