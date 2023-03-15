0c728e10a673c9f36d7211ec15f0c54fdb88cb91
zhangliangbo <2472110501@qq.com>
2023-03-15 15:59:57 +0800

    auto push 20230315

--- src/main/java/io/github/zhangliangbo/external/inner/downloader/ClientDownloadFileInitializer.java
+++ src/main/java/io/github/zhangliangbo/external/inner/downloader/ClientDownloadFileInitializer.java
@@ -3,6 +3,9 @@ package io.github.zhangliangbo.external.inner.downloader;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpContentDecompressor;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 /**
  * @author zhangliangbo
@@ -13,6 +16,7 @@ public class ClientDownloadFileInitializer extends ChannelInitializer<SocketChan
     @Override
     protected void initChannel(SocketChannel sc) throws Exception {
         ChannelPipeline pipeline = sc.pipeline();
+        pipeline.addLast("codec", new HttpClientCodec());
         pipeline.addLast(new ClientDownloadFileHandler());
     }
 
