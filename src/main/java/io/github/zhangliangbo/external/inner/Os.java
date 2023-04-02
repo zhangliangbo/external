9bcece5846672688aca69568520bc93379951572
zhangliangbo <2472110501@qq.com>
2023-04-02 23:22:48 +0800

    auto push 20230402

--- src/main/java/io/github/zhangliangbo/external/inner/Os.java
+++ src/main/java/io/github/zhangliangbo/external/inner/Os.java
@@ -1,17 +1,21 @@
 package io.github.zhangliangbo.external.inner;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArchUtils;
 
 /**
  * @author zhangliangbo
  * @since 2023/1/27
  */
+@Slf4j
 public class Os {
     public String arch() {
+        log.info("arch");
         return ArchUtils.getProcessor().getArch().getLabel();
     }
 
     public String type() {
+        log.info("type");
         return ArchUtils.getProcessor().getType().name();
     }
 }
