037bf152bafc13596c743ef21e4e806f356148a7
zhangliangbo <2472110501@qq.com>
2023-06-21 20:53:11 +0800

    auto push 20230621

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 .\mvnw clean native:compile -Pnative -DskipTests
-Move-Item -Path .\target\external.exe -Destination D:\scoop\shims -Force
+#Move-Item -Path .\target\external.exe -Destination D:\scoop\shims -Force
